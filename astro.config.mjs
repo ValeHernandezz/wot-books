6ae40c59037be3a44bdbe7b4757f25d249a0af0a
ValeHernandezz <valehernandezmodino@gmail.com>
2023-07-27 00:37:32 -0300

    Ahora sí

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import nodejs from '@astrojs/node';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  adapter: nodejs(),
+  output: 'hybrid',
 });
