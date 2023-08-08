9a4f82a2c171ef4035a57e588b48983823425eeb
ValeHernandezz <valehernandezmodino@gmail.com>
2023-08-08 20:05:23 -0300

    Nuevo tamaño de las imágenes

--- src/data/libros.js
+++ src/data/libros.js
@@ -2,7 +2,7 @@ export const libros = [
   {
     id: "el-ojo-del-mundo",
     titulo: "El Ojo del Mundo",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/imnsndxqof88j6ncv6yn",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/pe3b6aojt2undebhra0c",
     autor: "Robert Jordan",
     sinopsis:
       "La vida de Rand Al’Thor y sus amigos en Campo de Emond ha resultado bastante monótona hasta que una joven misteriosa llega al pueblo. Moraine, una maga capaz de encauzar el Poder Único, anuncia el despertar de una terrible amenaza.Esa misma noche, el pueblo se ve atacado por espantosos trollocs sedientos de sangre, unas bestias semihumanas que hasta entonces se habían considerado una leyenda. Mientras Campo de Emond soporta la ofensiva, Moraine y su guardián ayudan a Rand y a sus amigos a escapar.",
@@ -23,7 +23,7 @@ export const libros = [
   {
     id: "la-gran-caceria",
     titulo: "La Gran Cacería",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/mckqzgjwqzhpnzdspdlj",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/fyfhh6sqa0k0sr04ikc3",
     autor: "Robert Jordan",
     sinopsis:
       "¡Rand ha sobrevivido a su primer enfrentamiento con los perversos seguidores del Oscuro, pero ni sus amigos ni él están a salvo, ya que el señor del mal ha liberado a los Renegados, mientras los héroes de todas las eras se levantan de la tumba cuando el Cuerno de Valere los saca de su sueño! Al verse obligado a enfrentarse a las fuerzas de la oscuridad, Rand decide escapar de su destino. Pero la profecía tiene que cumplirse.",
@@ -44,7 +44,7 @@ export const libros = [
   {
     id: "el-dragon-renacido",
     titulo: "El Dragón Renacido",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/qfitzy8dwttdizhvuwpv",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/cvjxox0uepjlzkyxydkv",
     autor: "Robert Jordan",
     sinopsis:
       "Rand, acosado por inquietantes sueños sobre una espada de cristal, decide abandonar a sus compañeros tras un ataque de Engendros de la Sombra y se encamina hacia Tear para descubrir quién es realmente. Mientras tanto, las tres jóvenes aspirantes a Aes Sedai viajan con Mat hacia Tar Valon para ingresar como novicias en la Torre Blanca, donde esperan que las hermanas sanen a Mat de la extraña enfermedad que padece. Poco tiempo después, la Amyrlin les encomienda una peligrosa misión...",
@@ -65,7 +65,7 @@ export const libros = [
   {
     id: "el-ascenso-de-la-sombra",
     titulo: "El ascenso de la Sombra",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/skkktdvhcjaj1pqjdbv8",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/ytnn4tglfl5iflbkbsg7",
     autor: "Robert Jordan",
     sinopsis:
       "Los sellos de Shayol Ghul se han debilitado y la presencia del Oscuro se hace cada vez más evidente. En Tar Valon, Min es testigo de hechos portentosos que vaticinan un horrible futuro. Los Capas Blancas buscan en Dos Ríos a un hombre con los ojos dorados y siguen el rastro de Dragón Renacido. Mientras tanto, Rand al’Thor se dedica a tomar sus propias y sorprendentes decisiones. Perrin, acompañado de Faile, Loial y algunos Aiel, llega a Dos Ríos después de atravesar los Portales de Piedra. Allí se encuentran con los Trollocs que sirven al Oscuro y con los Capas Blancas, que tienen una forma muy especial de entender la defensa de la Luz. Mientras Elayne y Nynaeve parten hacia Tanchico siguiendo el rastro de las Aes Sedai del Ajah Negro que se llevaron numerosos angreal, Rand trata de reunir a todos los clanes de los Aiel. Con ello cumplirá parte de la profecía de Rhuidean.",
@@ -86,7 +86,7 @@ export const libros = [
   {
     id: "cielo-en-llamas",
     titulo: "Cielo en Llamas",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/efjxlr0fhyogg1xmukf7",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/dd8csmm3gkvzwbgizi3o",
     autor: "Robert Jordan",
     sinopsis:
       "Rand aguarda en Rhuidean a que se unan todos los clanes de los Aiel, pero la actitud del jefe de los Shaido puede obligar a Rand a cambiar de planes. En la corte de Caemlyn, Morgase tiene que enfrentarse a una traición inesperada, mientras la Torre Blanca se convierte en un nido de intrigas. Elaida, ascendida a Sede Amyrlin, pretende capturar al Dragón Renacido para mantenerlo a salvo hasta el momento del Tarmon Gaidon, aunque algunos creen que no es eso lo que pretende. El Dragón Renacido conduce a los Aiel hacia Cairhien, donde el jefe Shaido Couladin ha puesto cerco a la ciudad. Entretanto, Nynaeve y Elayne van a encontrarse con las hermanas Azules en Salidar, donde se han reunido las disidentes. Egwene, que continúa su formación con Moraine para llegar a ser una Aes Sedai, mantiene estrechamente vigilado a Rand. Y, de forma inexorable, los sellos que cierran la prisión del Oscuro se van abriendo.",
@@ -107,7 +107,7 @@ export const libros = [
   {
     id: "el-señor-del-caos",
     titulo: "El Señor del Caos",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/gwutspycyjobcinpdkot",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/ts4zvbcabuiqb3mzfvga",
     autor: "Robert Jordan",
     sinopsis: "Rand se esfuerza por unir a las naciones para combatir al Oscuro al tiempo que sortea las trampas que los Renegados tienden a la desprevenida raza humana. Pero además tiene que enfrentarse a los Hijos de la Luz, cuyo capitán general se propone desprestigiarlo y dirigir la batalla contra la Sombra. Por su parte, las Aes Sedai buscan a Rand para ofrecerle su apoyo, aunque éste sospecha que su verdadera intención es usarlo para sus propios fines.",
     linkCompra: "https://www.amazon.com/gp/product/844500705X/ref=x_gr_bb_amazon?ie=UTF8&tag=x_gr_bb_amazon-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=844500705X&SubscriptionId=1MGPYB6YW3HWK55XCGG2",
@@ -127,7 +127,7 @@ export const libros = [
   {
     id: "la-corona-de-espadas",
     titulo: "La Corona de Espadas",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/ysw00d97fjku1uqu8twv",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/toytjgqwkyjmusir4p0c",
     autor: "Robert Jordan",
     sinopsis: "Elayne, Nynaeve y Aviendha prosiguen con sus investigaciones en Ebou Dar para encontrar el ter’angreal con el que podrían frenar la espantosa ola de calor que azota el mundo. Para ello se reúnen con los Marinos, quienes dan el nombre de Cuenco de los Vientos al objeto. Egwene continúa con su lucha para ser la Sede Amyrlin escogida por las Aes Sedai rebeldes, y acabar con el control que sobre ella ejercen las Asentadas y otros grupos de hermanas. Entre tanto, en los distintos reinos, nobles y dirigentes continúan buscando su propio beneficio, sin tener presente que la mano del Oscuro está tocando el mundo y que la Última Batalla se acerca.",
     linkCompra: "https://www.amazon.com/gp/product/8445007068/ref=x_gr_bb_amazon?ie=UTF8&tag=x_gr_bb_amazon-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=8445007068&SubscriptionId=1MGPYB6YW3HWK55XCGG2",
@@ -147,7 +147,7 @@ export const libros = [
   {
     id: "el-camino-de-dagas",
     titulo: "El Camino de Dagas",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/gxlw1lh9jejsqm4fjxm5",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/agjuhtkjjzatkidgvhsy",
     autor: "Robert Jordan",
     sinopsis: "Mientras Elayne y Nynaeve tratan de hacer funcionar el Cuenco de los Vientos, Perrin se dirige a Ghealdan para que la reina Alliandre respalde públicamente al Dragón Renacido. Rand continúa en Illian, intentando pacificar el país. El tan deseado cambio climático, propiciado por el Cuenco de los Vientos, provoca bruscos cambios de temperatura que dificultan el desplazamiento de tropas.Rand decide hacer frente a los seanchan en las costas de Illian para frenar el avance del ejército invasor, pero además de al enemigo también tendrá que enfrentarse a la traición. ",
     linkCompra: "https://www.amazon.com/gp/product/8445007076/ref=x_gr_bb_amazon?ie=UTF8&tag=x_gr_bb_amazon-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=8445007076&SubscriptionId=1MGPYB6YW3HWK55XCGG2",
@@ -167,7 +167,7 @@ export const libros = [
   {
     id: "el-corazon-del-invierno",
     titulo: "El Corazón del invierno",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/l5g3i3jzhewwhtuo1ifv",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/lqfjmrytl32axgjfn1jc",
     autor: "Robert Jordan",
     sinopsis: "Tras el ataque en el Palacio del Sol, Rand viaja con Min dejando pistas falsas a sus perseguidores. En Cairhien, Cadsuane continúa preparando la estrategia para la Última Batalla. Perrin organiza el rescate de Faile y en Tar Valon se suceden las intrigas de la Torre Blanca. Los seanchan han conquistado Ebou Dar y Mat se encuentra atrapado allí junto a sus compañeros. Mientras se encuentra convaleciente, planea su fuga, para lo que aprovechará la ausencia de Tylin. La situación se complica cuando se ve forzado a ayudar a tres Aes Sedai a las que las Suldam todavía no han detectado.",
     linkCompra: "https://www.amazon.com/gp/product/8445007084?ref_=dbs_m_mng_rwt_calw_tpbk_8&storeType=ebooks",
@@ -187,7 +187,7 @@ export const libros = [
   {
     id: "encrucijada-en-el-crepusculo",
     titulo: "Encrucijada en el crepúsculo",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/lkuyzfwuavi5vsvkljaf",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/smxgaydvfblmufk1x81k",
     autor: "Robert Jordan",
     sinopsis: "Mat Cauthon huye con la hija de las Nueve Lunas mientras la Sombra y el imperio seanchan emprenden una persecución implacable. Por su parte, las Aes Sedai sienten un inmenso flujo de Poder en un lejano paraje del oeste y temen que sea obra de los Renegados o incluso de la propia Sombra. La heredera del Trono de Andor, rodeada de enemigos y de amigos siniestros que planean su destrucción, puede caer en manos de la Sombra y arrastrar consigo al Dragón Renacido, y Egwene al’Vere pone sitio al centro de poder Aes Sedai, pero ha de vencer con rapidez para evitar que los Asha’man sean los únicos capaces de defender el mundo del Oscuro. Tras limpiar la mitad masculina de la Fuente Verdadera, Rand al’Thor se ve obligado a correr grandes riesgos sin saber con certeza quiénes son sus aliados y quiénes son sus enemigos.",
     linkCompra: "https://www.amazon.com/Tiempo-Encrucijada-crepúsculo-Biblioteca-Spanish/dp/8445007092/ref=monarch_sidesheet",
@@ -207,7 +207,7 @@ export const libros = [
   {
     id: "cuchillo-de-sueños",
     titulo: "Cuchillo de sueños",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/kxo2uwphsojtvgebuzks",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/qhkd33xx0szagnknafsn",
     autor: "Robert Jordan",
     sinopsis: "Mientras el entramado de la realidad se vuelve inestable, todo indica que el Tarmon Gai’don está cerca y que Rand al’Thor tiene que enfrentarse con el Oscuro. Pero antes deberá negociar una tregua con los seanchan. Perrin, por su parte, ya ha hecho un pacto con ellos y está dispuesto a todo para salvar a su esposa de los Shaido. En Caemlyn, Elayne lucha para conseguir el Trono de León al tiempo que intenta prevenir una guerra civil, y Egwene descubre que incluso la Torre Blanca ha dejado de ser un lugar seguro. ",
     linkCompra: "https://www.amazon.com/gp/product/8445007106?ref_=dbs_m_mng_rwt_calw_tpbk_10&storeType=ebooks",
@@ -227,7 +227,7 @@ export const libros = [
   {
     id: "la-tormenta",
     titulo: "La tormenta",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/i5hbf5bewxi5hatyfdqa",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/rq8mlkk50mkv1yj8uiz8",
     autor: "Robert Jordan, Brandon Sanderson",
     sinopsis: "El Tarmon Gai’don, la Última Batalla, se cierne amenazadora y la humanidad no está preparada. Rand al’Thor, el Dragón Renacido, se esfuerza por conseguir la unión de reinos y alianzas para el enfrentamiento decisivo. Mientras frena la invasión seanchan hacia el norte —con la esperanza de conseguir al menos una tregua— sus aliados observan con espanto la sombra que parece crecer en el corazón del propio Dragón Renacido. Egwene al’Vere, la Sede Amyrlin de las Aes Sedai rebeldes, está cautiva en la Torre Blanca y sujeta a los caprichos de la tiránica dirigente. Su lucha pondrá a prueba el temple de las Aes Sedai, y el conflicto que plantea su presencia decidirá el futuro de la Torre Blanca… y quizás el del propio mundo.",
     linkCompra: "https://www.amazon.com/gp/product/8445007114?ref_=dbs_m_mng_rwt_calw_tpbk_11&storeType=ebooks",
@@ -247,7 +247,7 @@ export const libros = [
   {
     id: "torres-de-medianoche",
     titulo: "Torres de medianoche",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wfb0ptapjmr0nblmlanw",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/stc6zwl88bvdpqf8dxel",
     autor: "Robert Jordan, Brandon Sanderson",
     sinopsis: "La Última Batalla ha comenzado. El sol de la tercera era inicia el declive hacia el ocaso. Los Capas Blancas, un asesino de lobos y las responsabilidades del liderazgo acosan a Perrin Aybara. Debe buscar respuestas en el «Tel’aran’rhiod» a fin de superar estos obstáculos y controlar al lobo que lleva dentro y no perder para siempre su condición humana. Entre tanto, Matrim Cauthon se dispone a afrontar el reto más difícil de su vida: los seres del otro lado de los marcos de piedra, los alfinios y los elfinios. Confiaba en que ésta sería la última confrontación, pero la Rueda gira según sus designios. Se acerca el momento en que tendrá que vérselas de nuevo con las serpientes y los zorros, y participar en un juego en el que nunca se gana. La Torre de Ghenjei espera y sus secretos revelarán el destino de una persona amiga que lleva perdida mucho tiempo. «Dovie’andi se tovya sagain». Es hora de lanzar los dados.",
     linkCompra: "https://www.amazon.com/gp/product/8445007122?ref_=dbs_m_mng_rwt_calw_tpbk_12&storeType=ebooks",
@@ -267,7 +267,7 @@ export const libros = [
   {
     id: "un-recuerdo-de-luz",
     titulo: "Un recuerdo de luz",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/sq6aytc5lywncgtozqmu",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/at6gegxhb3o9hiazlr8v",
     autor: "Robert Jordan, Brandon Sanderson",
     sinopsis: "Los dirigentes de las naciones se reúnen en Campo de Merrilor para apoyar a Rand al’Thor o frenar su plan de romper los sellos de la prisión del Oscuro, algo que podría ser una señal de locura o la última esperanza de la humanidad. Egwene, la Sede Amyrlin, se inclina por lo primero. En Andor, los trollocs invaden Caemlyn. En el Sueño del Lobo, Perrin Aybara combate contra Verdugo. Mientras se aproxima a Ebou Dar, Mat Cauthon hace planes para visitar a su esposa, Tuon, ahora Fortuona, emperatriz de Seanchan. Toda la humanidad está en peligro, y el resultado se decidirá en Shayol Ghul. La Rueda gira, y la era actual llega a su fin. La Última Batalla determinará el destino del mundo.",
     linkCompra: "https://www.amazon.com/gp/product/8445007130?ref_=dbs_m_mng_rwt_calw_tpbk_13&storeType=ebooks",
@@ -287,7 +287,7 @@ export const libros = [
   {
     id: "nueva-primavera",
     titulo: "Nueva Primavera",
-    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/rjlmtgwitoue37kgdg0o",
+    img: "https://res.cloudinary.com/dzgkphroh/image/upload/f_auto,q_auto/v1/wot-covers/wot-covers/tol8jy5qqxgjqldk4c75",
     autor: "Robert Jordan",
     sinopsis: "La ciudad de Canluum es un refugio ante los peligros que acechan en el norte. Sin embargo, cuando Al'Lan Mandragoran, rey exiliado de Malkier y la mejor espada de su generación, llega a Canluum, se encuentra con un hervidero de rumores sobre los Engendros de la Sombra, lo que prueba que la fuerza del Oscuro crece y que la actividad de sus secuaces comienza a extenderse. Dentro de las murallas de Canluum, Lan conocerá a la mujer que determinará su destino. Moraine es una joven y poderosa Aes Sedai que ha viajado hasta la ciudad en busca de ayuda para una dificil misión: demostrar la verdad de una profecía en la que casi nadie cree sobre el modo de vencer a la sombra. ",
     linkCompra: "https://www.amazon.com/Nueva-Primavera-Rueda-del-tiempo/dp/8445006991/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1689910016&sr=1-1",
